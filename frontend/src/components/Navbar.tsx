18d3ae6f771bbc1d07dd23c5a365dc773bf44675
qbael <baohoo10205@gmail.com>
2025-10-07 22:29:48 +0700

    fix navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,44 +1,27 @@
 'use client'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import DropDownMenu from "@/components/ui/DropDownMenu";
 
 const Header = () => {
     return (
-        <header className='fixed top-0 left-0 flex items-center justify-between w-full h-16 px-3 bg-white shadow-md'>
+        <header className='fixed top-0 left-0 flex items-center justify-between w-full h-16 px-3 bg-white shadow-md z-50'>
             <div className='flex items-center'>
-                <Image 
-                    src='/logo.svg'
+                <Image
+                    src='logo.svg'
                     alt='logo'
-                    width={48}
-                    height={48}
+                    width={100}
+                    height={100}
                 />
             </div>
-            <div className='flex items-center gap-5'>
-                <div className='hover:cursor-pointer'>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Profile</DropdownMenuItem>
-                            <DropdownMenuItem>Billing</DropdownMenuItem>
-                            <DropdownMenuItem>Team</DropdownMenuItem>
-                            <DropdownMenuItem>Subscription</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+            <div className='flex items-center gap-1'>
+                <DropDownMenu/>
+                <div className='hover:cursor-pointer inline-flex justify-center rounded-md px-4 py-2 bg-white text-sm font-bold
+                 text-gray-700 hover:bg-blue-100 cursor-pointer mr-2'>
+                    Liên hệ
                 </div>
-                <div className='hover:cursor-pointer'>
-                    Đặt khám phòng khám
-                </div>
-                <Button variant='outline' className=' hover:cursor-pointer hover:bg-blue-500 hover:text-white'>
+                <Button variant='outline' className=' hover:cursor-pointer border-2 border-blue-500 shadow-2xs
+                text-blue-500 hover:bg-blue-500 hover:text-white'>
                     Đăng nhập
                 </Button>
             </div>
