d44598d763540ae53d73061804d92b7bfd3b0acd
ChimUng <duynguyen19087@gmail.com>
2025-10-09 14:06:06 +0700

    fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,8 +9,6 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import DoctorCard from "@/components/DoctorCard";
-import { healthyNav } from "@/lib/navItem";
-import SideBar from "@/components/Sidebar";
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
