259732005e99d7df3c2bcd91a0576e62dd26f594
qbael <baohoo10205@gmail.com>
2025-10-16 22:09:16 +0700

    fix clinic page

--- frontend/src/components/layout/ClinicCarousel.tsx
+++ frontend/src/components/layout/ClinicCarousel.tsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import {ChevronLeft, ChevronRight} from "lucide-react";
 import {ClinicsType} from "@/types/clinics-types";
 import {ClinicCard} from "@/components/ui/ClinicCard";
+import Link from "next/link";
 
 interface ClinicCarouselProps {
     clinics: ClinicsType[];
@@ -36,12 +37,15 @@ const ClinicCarousel = ({clinics}: ClinicCarouselProps) => {
             <div className="flex justify-between items-center mb-6 px-5">
                 <div>
                     <h2 className="text-2xl font-bold text-gray-900 mb-1">Đặt khám bệnh viện</h2>
-                    <p className="text-gray-600">Đặt khám và thanh toán dễ có phiếu khám (thời gian, số thứ tự) trước khi đi khám các bệnh viện kết nối chính thức với YouMed.</p>
+                    <p className="text-gray-600">Đặt khám và thanh toán dễ có phiếu khám (thời gian, số thứ tự) trước
+                        khi đi khám các bệnh viện kết nối chính thức với YouMed.</p>
                 </div>
-                <button
-                    className="bg-cyan-400 hover:bg-blue-700 text-sm font-bold text-white px-3 mt-7 py-1 rounded-lg flex items-center gap-2 transition-colors">
-                    Xem thêm <ChevronRight size={18}/>
-                </button>
+                <Link href="/booking/clinics">
+                    <button
+                        className="bg-cyan-400 hover:bg-blue-700 text-sm font-bold text-white px-3 mt-7 py-1 rounded-lg flex items-center gap-2 hover:cursor-pointer transition-colors">
+                        Xem thêm <ChevronRight size={18}/>
+                    </button>
+                </Link>
             </div>
 
             <div className="relative">
@@ -50,13 +54,13 @@ const ClinicCarousel = ({clinics}: ClinicCarouselProps) => {
                     disabled={clinicIndex === 0}
                     className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-4 bg-white rounded-full p-2 shadow-lg z-10 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-gray-50"
                 >
-                    <ChevronLeft size={24} />
+                    <ChevronLeft size={24}/>
                 </button>
 
                 <div className="overflow-hidden">
                     <div
                         className="flex gap-6 transition-transform duration-300"
-                        style={{ transform: `translateX(-${clinicIndex * (100 / visibleClinics)}%)` }}
+                        style={{transform: `translateX(-${clinicIndex * (100 / visibleClinics)}%)`}}
                     >
                         {clinics.map((clinic) => (
                             <ClinicCard key={clinic.id} {...clinic} />
@@ -69,12 +73,12 @@ const ClinicCarousel = ({clinics}: ClinicCarouselProps) => {
                     disabled={clinicIndex >= clinics.length - visibleClinics}
                     className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-4 bg-white rounded-full p-2 shadow-lg z-10 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-gray-50"
                 >
-                    <ChevronRight size={24} />
+                    <ChevronRight size={24}/>
                 </button>
             </div>
             {totalSlides > 1 && (
                 <div className="flex justify-center gap-2 mt-6">
-                    {Array.from({ length: totalSlides }).map((_, index) => (
+                    {Array.from({length: totalSlides}).map((_, index) => (
                         <button
                             key={index}
                             onClick={() => goToSlide(index)}
