0d357a730692d4b5cf034f11f4425392dba2b232
ChimUng <duynguyen19087@gmail.com>
2025-10-23 15:32:01 +0700

    fix

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -7,4 +7,3 @@ export function cn(...inputs: ClassValue[]) {
 
 export const DOCTORS_API_URL = "http://localhost:8081/api/v1/doctors"
 export const CLINICS_API_URL = "http://localhost:8081/api/v1/clinics"
-// export const SPECIALTIES_API_URL = "http://localhost:8081/api/v1/specialties"
